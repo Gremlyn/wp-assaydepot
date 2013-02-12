ccbac5e737463f9e43422fddc393be73f85a29aa
Gremlyn <gremlyn1@gmail.com>
2013-02-11 16:14:09 -0800

    Ready for a trial run with new oauth2 code

--- wp-assaydepot.php
+++ wp-assaydepot.php
@@ -6,25 +6,37 @@ Description: A plugin that allows the user to access information from
              the Assay Depot system and display it on a WordPress
              site. it is built using assaydepot-php, the PHP SDK for
              Assay Depot's JSON API.
-Version: 0.0
+Version: 0.1
 Author: Colin Burton
 Author URI: https://plus.google.com/u/0/117991393503416337615
 License: MIT LICENSE
 */
 
+require_once('ad-config.php');
 require_once('adphp/lib/assaydepot.php');
 
+require_once('PHP-OAuth2/Client.php');
+require_once('PHP-OAuth2/GrantType/IGrantType.php');
+require_once('PHP-OAuth2/GrantType/ClientCredentials.php');
+
+/**
+ * Make the connection to the API.
+ */
+$client = new OAuth2\Client(CLIENT_ID, CLIENT_SECRET, 1);
+$params = array();
+$response = $client->getAccessToken(TOKEN_ENDPOINT, 'client_credentials', $params);
+$access_token = $response['result']['access_token'];
+
+/**
+ * Set variables from query string, and sanitise while we're at it.
+ */
 $page = (get_query_var('page')) ? get_query_var('page') : '';
 if (!is_int($page) && $page != '') {
-    die('Non-integer specified for page number!');
+    $page = '';
 }
 
-// Set config variables for class
-$access_token = '';
-$url = 'https://www.assaydepot.com/api';
-
-// Instantiate the class
-$ad_api = new assaydepot($access_token, $url);
+$queryad = (get_query_var('queryad')) ? get_query_var('queryad') : '';
+$queryad = sanitize_text_field($queryad);
 
 /**
  * Results Processing and Display Functions
@@ -36,24 +48,23 @@ function ad_search_results($args) {
             'per_page' => '',
             'sort_by' => '',
             'sort_order' => '',
-            'query' => ''
+            'query' => $queryad
     ), $args) );
 
     // Instantiate the class
-    $ad_api = new assaydepot($access_token, $url);
-
+    $assaydepot = new assaydepot($access_token, AD_URL);
 
     // Set options, if they exist
-    ($args['page'] != '') ? $ad_api->option_set('page', $args['page']) : NULL;
-    ($args['per_page'] != '') ? $ad_api->option_set('per_page', $args['per_page']) : NULL;
-    ($args['sort_by'] != '') ? $ad_api->option_set('sort_by', $args['sort_by']) : NULL;
-    ($args['sort_order'] != '') ? $ad_api->option_set('sort_order', $args['sort_order']) : NULL;
+    ($args['page'] != '') ? $assaydepot->option_set('page', $args['page']) : NULL;
+    ($args['per_page'] != '') ? $assaydepot->option_set('per_page', $args['per_page']) : NULL;
+    ($args['sort_by'] != '') ? $assaydepot->option_set('sort_by', $args['sort_by']) : NULL;
+    ($args['sort_order'] != '') ? $assaydepot->option_set('sort_order', $args['sort_order']) : NULL;
 
     // Pass required args to search (builds the search URL, doesn't perform it)
-    $ad_api->search($args['type'], $args['query']);
+    $assaydepot->search($args['type'], $args['query']);
 
     // Make API call and receive back associative array with results
-    $search_output = $ad_api->json_output();
+    $search_output = $assaydepot->json_output();
 
 }
 add_shortcode('ad_search_results', 'ad_search_results');
