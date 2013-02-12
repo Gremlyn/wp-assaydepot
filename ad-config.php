c6f737d42e3c9ba1393718484d447edb69b95d58
Gremlyn <gremlyn1@gmail.com>
2013-02-11 18:11:18 -0800

    changed from improper use of 'const' outside of class to define() to create constants.

--- ad-config.php
+++ ad-config.php
@@ -1,9 +1,9 @@
 <?php
-const CLIENT_ID = 'enter client ID';
-const CLIENT_SECRET = 'enter client secret';
+define(CLIENT_ID, 'enter client ID');
+define(CLIENT_SECRET, 'enter client secret');
 
-const REDIRECT_URI           = 'http://location/of/this/index.php';
-const AUTHORIZATION_ENDPOINT = 'https://www.assaydepot.com/oauth/authorize';
-const TOKEN_ENDPOINT         = 'https://www.assaydepot.com/oauth/token';
+define(REDIRECT_URI, 'http://location/of/this/index.php');
+define(AUTHORIZATION_ENDPOINT, 'https://www.assaydepot.com/oauth/authorize');
+define(TOKEN_ENDPOINT, 'https://www.assaydepot.com/oauth/token');
 
-const AD_URL = 'https://www.assaydepot.com/api';
+define(AD_URL, 'https://www.assaydepot.com/api');
