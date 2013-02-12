768d6b3aa8f2f23dfcc075f0e84b014a79c793cc
Gremlyn <gremlyn1@gmail.com>
2013-02-11 16:35:01 -0800

    missing line ending ;

--- ad-config.php
+++ ad-config.php
@@ -6,4 +6,4 @@ const REDIRECT_URI           = 'http://location/of/this/index.php';
 const AUTHORIZATION_ENDPOINT = 'https://www.assaydepot.com/oauth/authorize';
 const TOKEN_ENDPOINT         = 'https://www.assaydepot.com/oauth/token';
 
-const AD_URL = 'https://www.assaydepot.com/api'
+const AD_URL = 'https://www.assaydepot.com/api';
