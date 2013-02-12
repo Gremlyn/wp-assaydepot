d8173a43ba929b2c68600e5de589a4a702e1a230
Gremlyn <gremlyn1@gmail.com>
2013-02-11 16:30:57 -0800

    Added URL to config file, also should later be an option to set from admin panel.

--- ad-config.php
+++ ad-config.php
@@ -5,3 +5,5 @@ const CLIENT_SECRET = 'enter client secret';
 const REDIRECT_URI           = 'http://location/of/this/index.php';
 const AUTHORIZATION_ENDPOINT = 'https://www.assaydepot.com/oauth/authorize';
 const TOKEN_ENDPOINT         = 'https://www.assaydepot.com/oauth/token';
+
+const AD_URL = 'https://www.assaydepot.com/api'
